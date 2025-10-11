028ce57ba78bb7fd65e1d9dc66e4ad22b37fe9e0
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-10-10 22:20:31 -0300

    Benchmarks para a lista ligada persistente feitos

--- Benchmark/Lista_Ligada/lista_ligada_pmem.go
+++ Benchmark/Lista_Ligada/lista_ligada_pmem.go
@@ -13,6 +13,7 @@ import (
 
 var(
 		arquivoPmem = flag.String("file","lista-ligada.goPool","arquivo de memória persistente")
+	workload = flag.String("workload","insert","tipo do workload: insert | update")
 )
 
 type Node struct {
@@ -40,6 +41,20 @@ func (ll *ListaLigada) insereComeco(data int){
 		}
 }
 
+func(ll *ListaLigada) atualizaAleatorio(){
+		if ll.tam == 0 {
+			return
+		}
+	pos := rand.Intn(ll.tam)
+	atual := ll.cabeca
+	for i := 0; i < pos && atual.prox != nil ; i++ {
+		atual = atual.prox
+	}
+	txn("undo"){
+		atual.data = rand.Intn(201)
+	}
+}
+
 func main() {
 		flag.Parse()
 		rand.Seed(time.Now().UnixNano())
@@ -55,16 +70,37 @@ func main() {
 			fmt.Println("Primeira inicialização do pool PMEM")
 		} else{
 				lista = (*ListaLigada)(pmem.Get("root",lista))
-		}
-
-		insercoes := 0 
 		
-			for {
-				valor := rand.Intn(201)
-				lista.insereComeco(valor)
-				insercoes++ 
-				if insercoes%1000 == 0{
-						fmt.Printf("Inserções até agora: %d\n",insercoes)
-			}
 		}
+		fmt.Printf("Executando workload: %s\n",*workload)
+		insercoes := 0 
+		atualizacoes := 0
+
+		switch *workload{
+			case "insert":
+				for {
+					valor := rand.Intn(201)
+					lista.insereComeco(valor)
+					insercoes++ 
+					if insercoes%1000 == 0{
+						fmt.Printf("Inserções até agora: %d\n",insercoes)
+					}
+				}
+			case "update":
+					for {
+							if rand.Float64() < 0.5 {
+									lista.insereComeco(rand.Intn(201)) //50% inserção 								 
+									insercoes++
+							} else {
+									lista.atualizaAleatorio() //50% atualização 
+									atualizacoes++ 
+							}
+						
+							if (insercoes+atualizacoes)%1000 == 0{
+									fmt.Printf("Ops: %d (insert: %d | update: %d)\n",insercoes+atualizacoes,insercoes,atualizacoes)
+						}
+					}
+			default:
+					fmt.Println("Workload inválido. Use -workload=insert ou -workload=update")
+		}	
 }
